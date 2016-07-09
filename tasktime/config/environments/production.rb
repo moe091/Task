5b81502d6b2e007ba7167a7b70fad6f74d826e46
moe091 <moe091@gmail.com>
2016-07-09 01:01:22 -0400

    Precompile assets

--- tasktime/config/environments/production.rb
+++ tasktime/config/environments/production.rb
@@ -14,11 +14,10 @@ Tasktime::Application.configure do
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
-  # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
 
-  # Generate digests for assets URLs
-  config.assets.digest = true
+  config.assets.compile = true
+config.assets.digest = true
+
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
